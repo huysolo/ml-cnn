3307aa47874d359682168967aaafb6b602539205
Thành Ngu96 <thanh.nguyen@mideasvn.com>
2018-11-12 21:00:22 +0700

    Update Code

--- demo.py
+++ demo.py
@@ -3,9 +3,6 @@ import gzip
 import pickle
 from time import time
 from numba import cuda, float32
-import os
-os.environ['NUMBAPRO_LIBDEVICE'] = "/usr/lib/nvidia-cuda-toolkit/libdevice"
-os.environ['NUMBAPRO_NVVM'] = "/usr/lib/x86_64-linux-gnu/libnvvm.so"
 
 epochs = 10
 neurons1 = 32
@@ -169,72 +166,70 @@ vw3 = np.zeros(w3.shape)
 mb3 = np.zeros(b3.shape)
 vb3 = np.zeros(b3.shape)
 
-for epoch_no in range(0,epochs):
-	num_correct = 0
-	start = time()
-	for index in range(0,60000):
-		o1 = np.zeros([26,26,8])
-		pro1 = np.zeros([26,26,8])
-		cuda_conv_forward[(8),(26,26)](data[index], f1, pro1)
-		o1 = reLU(pro1)
-		p1 = np.zeros([13,13,8])
-		cuda_pool_forward[(8),(13,13)](o1,p1)
-		pflat = p1.flatten().reshape(-1,1)
-		l1 = sigmoid(np.matmul(w1,pflat) + b1)
-		l2 = sigmoid(np.matmul(w2, l1) + b2)
-		l3 = sigmoid(np.matmul(w3, l2) + b3)
-		
-		if np.argmax(l3) == np.argmax(labels[index]):
-			num_correct += 1
-		
-		pic_loss = (labels[index] - l3)**2
-		
-		if (index+1)%100 == 0:
-			print(str(epoch_no) + ": " + str(index+1) + ": " + str(pic_loss.sum()))
-		
-		l3_grad = 2 * (l3 - labels[index])
-		sig3_grad = l3_grad * l3 * (1-l3)
-		b3_grad = sig3_grad
-		wx3_grad = sig3_grad
-		w3_grad = np.matmul(wx3_grad, np.transpose(l2))
-		l2_grad = np.matmul(np.transpose(w3), wx3_grad)
-		sig2_grad = l2_grad * l2 * (1-l2)
-		b2_grad = sig2_grad
-		wx2_grad = sig2_grad
-		w2_grad = np.matmul(wx2_grad, np.transpose(l1))
-		l1_grad = np.matmul(np.transpose(w2), wx2_grad)
-		sig1_grad = l1_grad * l1 * (1-l1)
-		b1_grad = sig1_grad
-		wx1_grad = sig1_grad
-		w1_grad = np.matmul(wx1_grad, np.transpose(pflat))
-		pflat_grad = np.matmul(np.transpose(w1), wx1_grad)
-		p_grad = pflat_grad.reshape(13,13,8)
-		o1_grad = np.zeros([26,26,8])
-		cuda_pool_backward[(8),(26,26)](p_grad, o1_grad)
-		pro1_grad = reLU_back(pro1,o1_grad)
-		dx = np.zeros([28,28,1])
-		f1_grad = np.zeros([8,3,3,1])
-		cuda_conv_backward[(8),(26,26)](data[index], f1, pro1_grad, dx, f1_grad)	
-		
-		f1_,mf1,vf1 = Adam(f1_grad,mf1,vf1)
-		f1 -= f1_
-		w1_,mw1,vw1 = Adam(w1_grad,mw1,vw1)
-		w1 -= w1_
-		b1_,mb1,vb1 = Adam(b1_grad,mb1,vb1)
-		b1 -= b1_
-		w2_,mw2,vw2 = Adam(w2_grad,mw2,vw2)
-		w2 -= w2_
-		b2_,mb2,vb2 = Adam(b2_grad,mb2,vb2)
-		b2 -= b2_
-		w3_,mw3,vw3 = Adam(w3_grad,mw3,vw3)
-		w3 -= w3_
-		b3_,mb3,vb3 = Adam(b3_grad,mb3,vb3)
-		b3 -= b3_		
-
-	print(str(epoch_no) + ": " + 'Training Accuracy: ' + str(num_correct/600) + '%')
-	print('Time taken: ' + str((time()-start)) + 's')
-	wts = [f1,w1,b1,w2,b2,w3,b3]
-	saveWeights(wts)
+num_correct = 0
+start = time()
+for index in range(0,60000):
+	o1 = np.zeros([26,26,8])
+	pro1 = np.zeros([26,26,8])
+	cuda_conv_forward[(8),(26,26)](data[index], f1, pro1)
+	o1 = reLU(pro1)
+	p1 = np.zeros([13,13,8])
+	cuda_pool_forward[(8),(13,13)](o1,p1)
+	pflat = p1.flatten().reshape(-1,1)
+	l1 = sigmoid(np.matmul(w1,pflat) + b1)
+	l2 = sigmoid(np.matmul(w2, l1) + b2)
+	l3 = sigmoid(np.matmul(w3, l2) + b3)
+	
+	if np.argmax(l3) == np.argmax(labels[index]):
+		num_correct += 1
+	
+	pic_loss = (labels[index] - l3)**2
+	
+	if (index+1)%100 == 0:
+		print(str(index+1) + ": " + str(pic_loss.sum()))
+	
+	l3_grad = 2 * (l3 - labels[index])
+	sig3_grad = l3_grad * l3 * (1-l3)
+	b3_grad = sig3_grad
+	wx3_grad = sig3_grad
+	w3_grad = np.matmul(wx3_grad, np.transpose(l2))
+	l2_grad = np.matmul(np.transpose(w3), wx3_grad)
+	sig2_grad = l2_grad * l2 * (1-l2)
+	b2_grad = sig2_grad
+	wx2_grad = sig2_grad
+	w2_grad = np.matmul(wx2_grad, np.transpose(l1))
+	l1_grad = np.matmul(np.transpose(w2), wx2_grad)
+	sig1_grad = l1_grad * l1 * (1-l1)
+	b1_grad = sig1_grad
+	wx1_grad = sig1_grad
+	w1_grad = np.matmul(wx1_grad, np.transpose(pflat))
+	pflat_grad = np.matmul(np.transpose(w1), wx1_grad)
+	p_grad = pflat_grad.reshape(13,13,8)
+	o1_grad = np.zeros([26,26,8])
+	cuda_pool_backward[(8),(26,26)](p_grad, o1_grad)
+	pro1_grad = reLU_back(pro1,o1_grad)
+	dx = np.zeros([28,28,1])
+	f1_grad = np.zeros([8,3,3,1])
+	cuda_conv_backward[(8),(26,26)](data[index], f1, pro1_grad, dx, f1_grad)	
+	
+	f1_,mf1,vf1 = Adam(f1_grad,mf1,vf1)
+	f1 -= f1_
+	w1_,mw1,vw1 = Adam(w1_grad,mw1,vw1)
+	w1 -= w1_
+	b1_,mb1,vb1 = Adam(b1_grad,mb1,vb1)
+	b1 -= b1_
+	w2_,mw2,vw2 = Adam(w2_grad,mw2,vw2)
+	w2 -= w2_
+	b2_,mb2,vb2 = Adam(b2_grad,mb2,vb2)
+	b2 -= b2_
+	w3_,mw3,vw3 = Adam(w3_grad,mw3,vw3)
+	w3 -= w3_
+	b3_,mb3,vb3 = Adam(b3_grad,mb3,vb3)
+	b3 -= b3_		
+print('Training Accuracy: ' + str(num_correct/600) + '%')
+print('Time taken: ' + str((time()-start)) + 's')
+wts = [f1,w1,b1,w2,b2,w3,b3]
+saveWeights(wts)
 
 num_correct = 0
 for index in range(0,10000):
